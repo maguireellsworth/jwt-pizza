2839243e774487346745f90e4af04c604346799a
maguire ellsworth <m3k4e@byu.edu>
2026-02-10 11:31:01 -0700

    added call to basicInit to all tests

--- tests/FrontEndTests.spec.ts
+++ tests/FrontEndTests.spec.ts
@@ -13,17 +13,19 @@ test('checks everything is correct on the homepage', async ({ page }) => {
 });
 
 test('order a pizza with a logged in user', async ({page}) => {
+  await basicInit(page);
+
   await page.goto('http://localhost:5173/');
   await page.getByRole('button', { name: 'Order now' }).click();
   await expect(page.locator('h2')).toContainText('Awesome is a click away');
-  await page.getByRole('combobox').selectOption('1');
+  await page.getByRole('combobox').selectOption('4');
   await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
   await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
   await expect(page.locator('form')).toContainText('Selected pizzas: 2');
   await page.getByRole('button', { name: 'Checkout' }).click();
   await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
   await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
   await page.getByRole('button', { name: 'Pay now' }).click();
@@ -33,6 +35,8 @@ test('order a pizza with a logged in user', async ({page}) => {
 })
 
 test('create a new store as a franchise', async ({page}) => {
+  await basicInit(page);
+
   await page.goto('http://localhost:5173/');
   await page.getByRole('link', { name: 'Login' }).click();
   await page.getByRole('textbox', { name: 'Email address' }).fill('f@jwt.com');
